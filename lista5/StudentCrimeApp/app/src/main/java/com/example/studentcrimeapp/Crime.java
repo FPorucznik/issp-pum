043a53a5474d0fed42b5b85ea970a9cfd7a5fdab
FPorucznik <porucznik199@wp.pl>
2021-12-16 21:55:15 +0100

    initial commit

---
+++ lista5/StudentCrimeApp/app/src/main/java/com/example/studentcrimeapp/Crime.java
@@ -0,0 +1,36 @@
+package com.example.studentcrimeapp;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public void setTitle(String title) {
+        this.mTitle = title;
+    }
+
+    public boolean getSolved(){ return this.mSolved; }
+
+    public void setSolved(boolean solved) {
+        this.mSolved = solved;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id){ this.mId = id; }
+
+    public String getTitle() { return mTitle; }
+
+    public Date getDate() { return mDate; }
+
+    public void setDate(Date date){ this.mDate = date; }
+}
---
+++ lista5/StudentCrimeApp/app/src/main/java/com/example/studentcrimeapp/CrimeListAdapter.java
@@ -0,0 +1,90 @@
+package com.example.studentcrimeapp;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeListAdapter extends RecyclerView.Adapter<CrimeListAdapter.CrimeViewHolder> {
+
+    public final List<Crime> crimes;
+    private LayoutInflater inflater;
+    private Context context;
+
+    public CrimeListAdapter(Context context, List<Crime> crimes){
+        inflater = LayoutInflater.from(context);
+        this.crimes = crimes;
+        this.context = context;
+    }
+
+    class CrimeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public final TextView crimeName;
+        public final TextView crimeDetails;
+        public final ImageView crimeSolved;
+
+        final CrimeListAdapter adapter;
+
+        public CrimeViewHolder(@NonNull View itemView, CrimeListAdapter adapter) {
+            super(itemView);
+            crimeName = itemView.findViewById(R.id.crime_name);
+            crimeDetails = itemView.findViewById(R.id.crime_details);
+            crimeSolved = itemView.findViewById(R.id.crimeSolvedIcon);
+            this.adapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getLayoutPosition();
+            Crime current = crimes.get(position);
+            Intent intent = new Intent(context, CrimeActivity.class);
+            intent.putExtra("id", current.getId().toString());
+            context.startActivity(intent);
+            adapter.notifyItemChanged(position);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public CrimeListAdapter.CrimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = inflater.inflate(R.layout.crime_list_item, parent, false);
+        return new CrimeViewHolder(itemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CrimeListAdapter.CrimeViewHolder holder, int position) {
+        Crime current = crimes.get(position);
+        holder.crimeName.setText(current.getTitle());
+        DateFormat df = new SimpleDateFormat("E MMM dd HH:mm:ss zzz yyyy");
+        holder.crimeDetails.setText(df.format(current.getDate()));
+        if(current.getSolved()){
+            holder.crimeSolved.setVisibility(View.INVISIBLE);
+        }
+        else{
+            holder.crimeSolved.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return crimes.size();
+    }
+
+}
